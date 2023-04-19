4adcd2f23f3e138bd431cf0e8d5621cabcf77cd0
bakastrumpf <baka.strumpf@gmail.com>
2023-04-19 18:21:24 +0200

    message with attachment updated

--- src/main/java/com/iktpreobuka/emailexampletwo/models/MessageWithAttachment.java
+++ src/main/java/com/iktpreobuka/emailexampletwo/models/MessageWithAttachment.java
@@ -8,7 +8,7 @@ public class MessageWithAttachment {
 	private String subject;
 	private String text;
 	private File file; 
-	// private File attachment; 
+	// private body attachment; 
 	
 	
 	public MessageWithAttachment() {
